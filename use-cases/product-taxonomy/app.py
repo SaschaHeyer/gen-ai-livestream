30c6936ba048a15ac0a6d720f995a856b72cc8b1
Sascha Heyer <heyersaschastud@googlemail.com>
2024-10-23 11:57:09 +0200

    added caching to the taxonomy example

--- use-cases/product-taxonomy/app.py
+++ use-cases/product-taxonomy/app.py
@@ -2,47 +2,96 @@ import streamlit as st
 import base64
 import vertexai
 import requests
-import time  # Import to measure response time
-from vertexai.generative_models import GenerativeModel, Part, SafetySetting, GenerationConfig
+import time
+import datetime  # For caching TTL
+from vertexai.preview import caching  # Correct import for caching
+from vertexai.preview.generative_models import GenerativeModel, Part, SafetySetting, GenerationConfig  # Correct preview import
 
 RESPONSE_SCHEMA = {
-  "type": "object",
-  "properties": {
-    "taxonomy_hierarchy": {
-      "type": "string",
-      "description": "The full hierarchical taxonomy classification for the product."
-    }
-  },
-  "required": ["taxonomy_hierarchy"]
+    "type": "object",
+    "properties": {
+        "taxonomy_hierarchy": {
+            "type": "string",
+            "description": "The full hierarchical taxonomy classification for the product."
+        }
+    },
+    "required": ["taxonomy_hierarchy"]
 }
-
-
 
 # Initialize the Vertex AI client
 def initialize_vertex_ai():
     vertexai.init(project="sascha-playground-doit", location="us-central1")
 
-# Function to generate the content using Vertex AI and retrieve metadata
-def generate_content(model_name, text_input, system_instruction):
-    model = GenerativeModel(
-        model_name,
-        system_instruction=[system_instruction]
-    )
+# Function to generate content using Vertex AI and retrieve metadata
+def generate_content(model_name, text_input, system_instruction, use_cache, taxnomy):
     
-    # Generate content with metadata (stream=False to get full metadata)
-    start_time = time.time()  # Start the timer to measure response time
-    responses = model.generate_content(
-        [text_input],
-        generation_config=GenerationConfig(
-            temperature=1.0,
-            max_output_tokens=8192,
-            response_mime_type="application/json",
-            response_schema=RESPONSE_SCHEMA
-        ),
-        safety_settings=safety_settings,
-        stream=False,
-    )
-    end_time = time.time()  # End the timer to measure response time
+    
+    if use_cache:
+        # Show message to indicate cache generation
+        st.write("Generating cache...")  
+        
+        #st.write(system_instruction)
+      
+        
+        
+        #st.write(contents)
+        cached_content = caching.CachedContent.create(
+            model_name=model_name,
+            system_instruction=system_instruction,
+            contents=[Part.from_text(taxnomy)],
+            ttl=datetime.timedelta(minutes=60),
+        )
+        
+        
+        cached_content = caching.CachedContent(cached_content_name=cached_content.name)
+
+        
+        #st.write(cached_content)
+        
+        # Cache generation done, proceed to content generation
+        st.write("Cache generated. Now generating content...")  
+
+        # Use cached content for the model
+        model = GenerativeModel.from_cached_content(cached_content=cached_content)
+        
+        start_time = time.time()  # Start the timer to measure response time
+        print(text_input)
+        responses = model.generate_content(
+            [text_input],
+            generation_config=GenerationConfig(
+                temperature=1.0,
+                max_output_tokens=8192,
+                response_mime_type="application/json",
+                response_schema=RESPONSE_SCHEMA
+            ),
+            safety_settings=safety_settings,
+            stream=False,
+        )
+        end_time = time.time()  # End the timer to measure response time
+    else:
+        model = GenerativeModel(
+            model_name=model_name,
+            system_instruction=[system_instruction]
+        )
+        st.write("Generating content...")  # No caching, just generate
+        
+           # Generate content with metadata
+        start_time = time.time()  # Start the timer to measure response time
+        print(text_input)
+        responses = model.generate_content(
+            [text_input, Part.from_text(taxnomy)],
+            generation_config=GenerationConfig(
+                temperature=1.0,
+                max_output_tokens=8192,
+                response_mime_type="application/json",
+                response_schema=RESPONSE_SCHEMA
+            ),
+            safety_settings=safety_settings,
+            stream=False,
+        )
+        end_time = time.time()  # End the timer to measure response time
+
+ 
     
     response_time_ms = (end_time - start_time) * 1000  # Convert time to milliseconds
     return responses, response_time_ms
@@ -59,16 +108,16 @@ def fetch_taxonomy(url):
 
 # Function to calculate costs based on token counts
 def calculate_cost(prompt_token_count, candidates_token_count):
-    input_cost = (prompt_token_count / 1000) * 0.00001875  # Input cost rate
-    output_cost = (candidates_token_count / 1000) * 0.000075  # Output cost rate
-    total_cost = input_cost + output_cost
-    return input_cost, output_cost, total_cost
+    cost_per_1k_chars = 0.0000046875
+    total_chars = prompt_token_count + candidates_token_count
+    total_cost = (total_chars / 1000) * cost_per_1k_chars
+    return total_cost
 
 # Streamlit widescreen layout
 st.set_page_config(layout="wide")
 
 # Streamlit UI components
-st.title("Product Taxonomy Classifier")
+st.title("Product Taxonomy Classifier with Context Caching")
 
 st.markdown("""
     Application by [Sascha Heyer](https://www.linkedin.com/in/saschaheyer/)
@@ -128,21 +177,23 @@ safety_settings = [
 # Model selection (assuming the user might want to choose different versions)
 model_name = st.selectbox("Choose Model", options=["gemini-1.5-flash-002"])
 
+# Checkbox to enable context caching
+use_cache = st.checkbox("Enable Context Caching")
+
 # Button to trigger generation
 if st.button("Classify product"):
     # Ensure taxonomy_text is loaded or provide a default instruction
     if st.session_state.taxonomy_text is None:
         st.error("Please load the taxonomy before generating content.")
     else:
-        system_instruction = f"As a product taxonomy expert based on a predefined product taxonomy you are categorizing products either based on the image and or the product description and potential additional metadata.\n\n{st.session_state.taxonomy_text}"
+        system_instruction = f"As a product taxonomy expert based on a predefined product taxonomy you are categorizing products either based on the image and or the product description and potential additional metadata.\n\n"
         
-        st.write("Generating content...")
-
         # Initialize Vertex AI
         initialize_vertex_ai()
+    
 
         # Generate content and retrieve metadata and response time
-        responses, response_time_ms = generate_content(model_name, text_input, system_instruction)
+        responses, response_time_ms = generate_content(model_name, text_input, system_instruction, use_cache, st.session_state.taxonomy_text)
 
         # Display generated content prominently as "Classified Taxonomy"
         generated_text = responses.candidates[0].content.parts[0].text
@@ -155,7 +206,11 @@ if st.button("Classify product"):
         total_token_count = responses.usage_metadata.total_token_count
 
         # Calculate cost
-        input_cost, output_cost, total_cost = calculate_cost(prompt_token_count, candidates_token_count)
+        total_cost = calculate_cost(prompt_token_count, candidates_token_count)
+
+        # Calculate cost savings for context caching
+        cost_saved = total_cost * 0.75  # 75% cost savings
+        remaining_cost = total_cost * 0.25  # 25% remaining cost
 
         # Create 3 columns for token info, cost, and response time
         col1, col2, col3 = st.columns(3)
@@ -170,9 +225,12 @@ if st.button("Classify product"):
         # Column 2: Cost Calculation
         with col2:
             st.subheader("Cost Calculation:")
-            st.write(f"Input Cost: ${input_cost:.6f}")
-            st.write(f"Output Cost: ${output_cost:.6f}")
             st.write(f"Total Cost: ${total_cost:.6f}")
+
+            if use_cache:
+                st.write("**Context Caching Enabled**")
+                st.write(f"Cost saved due to context caching: ${cost_saved:.6f}")
+                st.write(f"Remaining cost after caching: ${remaining_cost:.6f}")
 
         # Column 3: Response Time
         with col3:
