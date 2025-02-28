399ccafe4cbfb8f43bc7d0295900cf56bf0c3215
Sascha Heyer <heyersaschastud@googlemail.com>
2025-02-28 17:34:08 +0100

    added livestream schedule website (why not =))

---
+++ schedule/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_from_directory('static', 'schedule.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory('static', path)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
