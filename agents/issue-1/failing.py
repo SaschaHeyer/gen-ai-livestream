ffe49e66ee8fe69c97b7abeb06c52453a0ef8be3
Sascha Heyer <heyersaschastud@googlemail.com>
2025-03-04 15:59:08 +0100

    added reasoning engine issue for google team reproduction

---
+++ agents/issue-1/failing.py
@@ -0,0 +1,31 @@
+import vertexai
+from vertexai.preview import reasoning_engines
+import base64
+
+STAGING_BUCKET = "gs://doit-llm"
+PROJECT_ID = "sascha-playground-doit"
+LOCATION = "us-central1"
+
+vertexai.init(project=PROJECT_ID, location=LOCATION, staging_bucket=STAGING_BUCKET)
+
+model = "gemini-2.0-pro-exp-02-05"
+agent = reasoning_engines.LangchainAgent(
+    model=model,
+    system_instruction="""
+    you behave like a pirate, every answer you response like a pirate
+    """,
+)
+
+remote_agent = reasoning_engines.ReasoningEngine.create(
+    agent,
+    requirements=["google-cloud-aiplatform[langchain,reasoningengine]"],
+)
+print(remote_agent)
+
+response = remote_agent.query(
+        input=f"Who are you?"
+    )
+
+print(response)
+
+
