7fe0952c1e57775261ce504f40fd3b8f73454174
Sascha Heyer <heyersaschastud@googlemail.com>
2025-03-25 16:56:05 +0200

    added next page

--- schedule/main.py
+++ schedule/main.py
@@ -2,9 +2,16 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return send_from_directory('static', 'schedule.html')
+
+
+@app.route('/next25')
+def next2025():
+    return send_from_directory('static', 'next.html')
+
 
 @app.route('/<path:path>')
 def static_files(path):
