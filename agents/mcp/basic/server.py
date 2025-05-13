37a50047db412d8678789340d567bb3852e04515
Sascha Heyer <heyersaschastud@googlemail.com>
2025-05-13 10:39:19 +0200

    added basic mcp server for future article

---
+++ agents/mcp/basic/server.py
@@ -0,0 +1,17 @@
+# server.py
+from fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
