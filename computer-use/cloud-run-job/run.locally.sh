2118b3432c804b2a4a477ccc90d197f26c76e30c
Sascha Heyer <heyersaschastud@googlemail.com>
2025-10-08 15:13:10 +0200

    fixed local run sh

--- computer-use/cloud-run-job/run.locally.sh
+++ computer-use/cloud-run-job/run.locally.sh
@@ -1,5 +1,3 @@
-PLAYWRIGHT_HEADLESS=false python job_main.py --target-url=https://browser-use-demo-24173060393.us-
-  central1.run.app/ \
-      --pdf-uri=/Users/sascha/Desktop/development/gen-ai-livestream/computer-use/cloud-run-job/
-  sample.pdf \
+PLAYWRIGHT_HEADLESS=false python job_main.py --target-url=https://browser-use-demo-24173060393.us-central1.run.app/ \
+      --pdf-uri=/Users/sascha/Desktop/development/gen-ai-livestream/computer-use/cloud-run-job/sample.pdf \
       --note-text="test"
